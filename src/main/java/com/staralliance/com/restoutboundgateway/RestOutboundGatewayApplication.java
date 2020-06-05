55a71233da91c7593811d7652a4b8f03dec41667
NasirAK90 <66468799+NasirAK90@users.noreply.github.com>
2020-06-05 20:09:11 +0530

    Added Initial Files

---
+++ src/main/java/com/staralliance/com/restoutboundgateway/RestOutboundGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.staralliance.com.restoutboundgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOutboundGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOutboundGatewayApplication.class, args);
+	}
+
+}
