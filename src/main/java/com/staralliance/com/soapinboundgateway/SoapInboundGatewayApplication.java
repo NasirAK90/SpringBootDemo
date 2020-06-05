8c245464eb6397cec0ae3ff84819e252e9ffecdf
NasirAK90 <nasirak90@yahoo.com>
2020-06-06 00:07:08 +0530

    update the package in java file

--- src/main/java/com/staralliance/com/soapinboundgateway/SoapInboundGatewayApplication.java
+++ src/main/java/com/staralliance/com/soapinboundgateway/SoapInboundGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.staralliance.com.restoutboundgateway;
+package com.staralliance.com.soapinboundgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
